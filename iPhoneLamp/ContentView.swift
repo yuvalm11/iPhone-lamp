dbe36774b2b3c207a78302a7e076b01c9f1cc5af
Yuval Mamana <yuvalm11@mit.edu>
2025-12-15 16:05:05 +0200

    Initial Commit

---
+++ iPhoneLamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iPhoneLamp
+//
+//  Created by Yuval Mamana on 15/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iPhoneLamp/iPhoneLampApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPhoneLampApp.swift
+//  iPhoneLamp
+//
+//  Created by Yuval Mamana on 15/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct iPhoneLampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
